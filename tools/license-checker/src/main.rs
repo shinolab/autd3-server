d20a130f5c9c4debe7e92b7d89ee5536c4a9ac5d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-23 07:32:25 +0900

    update

--- tools/license-checker/src/main.rs
+++ tools/license-checker/src/main.rs
@@ -1,181 +1,29 @@
-mod diff;
+use std::{io::Write, path::Path};
+
 mod npm;
 mod rs;
 
-use std::{
-    fs::{self, File},
-    io::{BufReader, BufWriter, Read, Write},
-    path::Path,
-};
-
-#[derive(Debug)]
-pub struct Dependency {
-    pub name: String,
-    pub version: String,
-    pub repository: Option<String>,
-    pub license: Option<String>,
-    pub license_file: Option<String>,
-}
-
 fn main() -> anyhow::Result<()> {
-    let npm_deps = npm::collect_npm_deps(
-        Path::new(env!("CARGO_MANIFEST_DIR")).join("../../node_modules"),
-        Path::new(env!("CARGO_MANIFEST_DIR")).join("../../package-lock.json"),
-    )?;
+    let root = Path::new(env!("CARGO_MANIFEST_DIR")).join("../..");
 
-    let rs_deps = rs::collect_rs_deps(
-        Path::new(env!("CARGO_MANIFEST_DIR")).join("../../src-tauri/Cargo.toml"),
-    )?;
-
-    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("../../..");
-
-    let old = path.parent().unwrap().join("NOTICE");
-    let new = path.parent().unwrap().join("NOTICE-new");
-
-    let mut writer = BufWriter::new(File::create(&new)?);
-
-    writeln!(
-        writer,
-        r"THIRD-PARTY SOFTWARE NOTICES AND INFORMATION
-
-This software includes the following third-party components.
-The license terms for each of these components are provided later in this notice.
-"
-    )?;
-
-    let mut licenses = std::collections::HashSet::new();
-    npm_deps
-        .into_iter()
-        .chain(rs_deps.into_iter())
-        .try_for_each(|dep| -> anyhow::Result<()> {
-            writeln!(
-                writer,
-                r"---------------------------------------------------------
-"
-            )?;
-
-            if dep.license.is_none() && dep.license_file.is_none() {
-                return Err(anyhow::anyhow!(
-                    "No license information found for {} {}",
-                    dep.name,
-                    dep.version
-                ));
-            }
-
-            writeln!(
-                writer,
-                "{} {}{}",
-                dep.name,
-                dep.version,
-                if let Some(license) = dep.license {
-                    let license = if license == "Apache-2.0 OR LGPL-2.1-or-later OR MIT" {
-                        "Apache-2.0 OR MIT".to_string()
-                    } else {
-                        license
-                    };
-                    license
-                        .split("AND")
-                        .flat_map(|token| {
-                            token
-                                .split("OR")
-                                .map(|token| token.trim().trim_matches(&['(', ')']))
-                        })
-                        .for_each(|license| {
-                            licenses.insert(license.to_string());
-                        });
-                    format!(" ({})", license)
-                } else {
-                    "".to_string()
-                }
-            )?;
-            if let Some(rep) = dep.repository {
-                writeln!(writer, "{}", rep)?;
-            }
-
-            if let Some(license_file) = dep.license_file {
-                writeln!(
-                    writer,
-                    r"
----
-            {}",
-                    license_file
-                )?;
-            }
-
-            Ok(())
-        })?;
-
-    writeln!(
-        writer,
-        r"---------------------------------------------------------
-        
-LICENSE TERMS"
-    )?;
-
-    let mut licenses = licenses.into_iter().collect::<Vec<_>>();
-    licenses.sort();
-    licenses
-        .into_iter()
-        .filter(|license| license != "Unlicense")
-        .try_for_each(|license| -> anyhow::Result<()> {
-            let path = Path::new(env!("CARGO_MANIFEST_DIR"))
-                .join("licenses")
-                .join(&license);
-
-            writeln!(
-                writer,
-                r"
----------------------------------------------------------
-{}
----
-",
-                license
-            )?;
-            let mut file_content = String::new();
-            if let Ok(mut reader) = fs::File::open(path).map(BufReader::new) {
-                reader.read_to_string(&mut file_content)?;
-            } else {
-                return Err(anyhow::anyhow!("License file not found for {}", license));
-            }
-            writer.write_all(file_content.as_bytes())?;
-
-            Ok(())
-        })?;
-
-    writeln!(
-        writer,
-        r"
----------------------------------------------------------"
-    )?;
-
-    writer.flush()?;
-
-    let changed = if old.exists() {
-        let mut old_license = String::new();
-        fs::File::open(&old)
-            .map(BufReader::new)?
-            .read_to_string(&mut old_license)?;
-        let old_license = old_license.replace("\r", "");
-
-        let mut new_license = String::new();
-        fs::File::open(&new)
-            .map(BufReader::new)?
-            .read_to_string(&mut new_license)?;
-        let new_license = new_license.replace("\r", "");
-
-        diff::show_diff(&old_license, &new_license)
-    } else {
-        false
-    };
-
-    std::fs::remove_file(&old)?;
-    std::fs::rename(new, old)?;
-
-    if changed {
-        return Err(anyhow::anyhow!(
-            "Some NOTICE files have been updated. Manuall check is required.",
-        ));
+    let mut file = std::fs::File::create(root.join("NOTICE"))?;
+    {
+        let mut writer = std::io::BufWriter::new(&mut file);
+        writeln!(writer, "THIRD-PARTY SOFTWARE NOTICES AND INFORMATION")?;
+        writeln!(writer)?;
+        writeln!(
+            writer,
+            "This software includes the following third-party components."
+        )?;
+        writeln!(writer)?;
+        writeln!(writer)?;
     }
+
+    let npm_deps = npm::get_npm_deps(root.join("node_modules"), root.join("package-lock.json"))?;
+    file.write_all(npm_deps.as_bytes())?;
+
+    let rs_deps = rs::get_rs_deps(&root)?;
+    file.write_all(rs_deps.as_bytes())?;
 
     Ok(())
 }
--- tools/license-checker/src/rs.rs
+++ tools/license-checker/src/rs.rs
@@ -1,79 +1,254 @@
-use std::{
-    io::Read,
-    path::{Path, PathBuf},
-};
+use std::fs;
+use std::io::Write;
+use std::{collections::BTreeMap, path::Path, sync::Arc};
 
-use crate::Dependency;
-use cargo_license::{get_dependencies_from_cargo_lock, GetDependenciesOpt};
-use cargo_metadata::MetadataCommand;
+use cargo_about::{Krate, get_all_crates, licenses::config::Config};
+use itertools::*;
+use krates::{LockOptions, Utf8Path};
+use semver::Version;
+use spdx::Licensee;
 
-pub fn collect_rs_deps<P>(path: P) -> anyhow::Result<Vec<Dependency>>
-where
-    P: Into<PathBuf>,
-{
-    let mut cmd = MetadataCommand::new();
+fn get_real_deps(dir: impl AsRef<Path>) -> anyhow::Result<Vec<(String, Version)>> {
+    let cargo_tree = std::process::Command::new("cargo")
+        .args(["tree", "--prefix", "none", "-e", "no-build", "-e", "no-dev"])
+        .current_dir(dir)
+        .output()?;
 
-    let path: PathBuf = path.into();
-    cmd.manifest_path(&path);
+    if !cargo_tree.status.success() {
+        anyhow::bail!(
+            "cargo tree failed: {}",
+            String::from_utf8_lossy(&cargo_tree.stderr)
+        );
+    }
 
-    let get_opts = GetDependenciesOpt {
-        avoid_dev_deps: true,
-        avoid_build_deps: true,
-        direct_deps_only: false,
-        root_only: false,
-    };
-
-    let license_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("src")
-        .join("license_file");
-
-    get_dependencies_from_cargo_lock(cmd, get_opts)?
-        .into_iter()
-        .map(|dep| {
-            let repo = dep.repository;
-            let license_file = dep
-                .license_file
-                .map(|license| -> anyhow::Result<String> {
-                    let path = license_dir.join(&dep.name);
-                    if std::fs::File::open(&path).is_err() {
-                        let paths = repo
-                            .as_ref()
-                            .unwrap()
-                            .trim_start_matches("https://")
-                            .split('/')
-                            .collect::<Vec<_>>();
-                        match paths[0] {
-                            "github.com" => {
-                                let url = format!(
-                                    "https://raw.githubusercontent.com/{}/{}/refs/heads/main/{}",
-                                    paths[1], paths[2], license
-                                );
-                                let body = reqwest::blocking::get(&url)?.text()?;
-                                std::fs::create_dir_all(&license_dir)?;
-                                std::fs::write(&path, &body)?;
-                            }
-                            _ => {
-                                return Err(anyhow::anyhow!(
-                                    "not supported repository: {}",
-                                    repo.as_ref().unwrap()
-                                ));
-                            }
-                        }
-                    }
-                    let file = std::fs::File::open(&path)?;
-                    let mut reader = std::io::BufReader::new(file);
-                    let mut license = String::new();
-                    reader.read_to_string(&mut license)?;
-                    Ok(license)
-                })
-                .transpose()?;
-            Ok(Dependency {
-                name: dep.name,
-                version: dep.version.to_string(),
-                license: dep.license,
-                license_file: license_file,
-                repository: repo,
-            })
+    let stdout = String::from_utf8_lossy(&cargo_tree.stdout);
+    stdout
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() < 2 {
+                anyhow::bail!("unexpected line: {}", line);
+            }
+            Ok((parts[0].to_owned(), Version::parse(&parts[1][1..])?))
         })
-        .collect()
+        .collect::<Result<Vec<_>, _>>()
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct DepWorkaround {
+    dep: Vec<Dep>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Dep {
+    host: Option<String>,
+    name: String,
+    path: Option<String>,
+    tag: Option<String>,
+    branch: Option<String>,
+}
+
+impl DepWorkaround {
+    fn load() -> anyhow::Result<Self> {
+        Ok(toml::from_str(&fs::read_to_string("workaround.toml")?)?)
+    }
+
+    fn find(&self, writer: &mut impl Write, krate: &Krate) -> anyhow::Result<()> {
+        if let Some(d) = self.dep.iter().find(|d| d.name == krate.name) {
+            let repo = krate
+                .repository
+                .as_ref()
+                .ok_or_else(|| anyhow::anyhow!("no repository found for crate {}", krate.name))?
+                .trim_end_matches(".git");
+            let repo = match &d.host {
+                Some(host) if host == "gitlab" => repo.to_owned(),
+                None => repo.replace("github.com", "raw.githubusercontent.com"),
+                _ => {
+                    anyhow::bail!(
+                        "unsupported host '{}' for crate {}",
+                        d.host.as_ref().unwrap(),
+                        krate.name
+                    );
+                }
+            };
+            let tag = d
+                .tag
+                .as_ref()
+                .map(|tag| tag.replace("${VERSION}", &krate.version.to_string()));
+            let path = if let Some(path) = &d.path {
+                path
+            } else {
+                return Ok(());
+            };
+            let url = match &d.host {
+                Some(host) if host == "gitlab" => {
+                    if let Some(tag) = tag {
+                        format!("{}/-/raw/{}/{}", repo, tag, path)
+                    } else {
+                        format!(
+                            "{}/-/raw/{}/{}",
+                            repo,
+                            d.branch.as_deref().unwrap_or("master"),
+                            path
+                        )
+                    }
+                }
+                None => {
+                    if let Some(tag) = tag {
+                        format!("{}/refs/tags/{}/{}", repo, tag, path)
+                    } else {
+                        format!(
+                            "{}/refs/heads/{}/{}",
+                            repo,
+                            d.branch.as_deref().unwrap_or("master"),
+                            path
+                        )
+                    }
+                }
+                _ => unreachable!(),
+            };
+
+            let res = reqwest::blocking::get(&url)?;
+            if !res.status().is_success() {
+                anyhow::bail!(
+                    "failed to fetch license file from {}: {}",
+                    url,
+                    res.status()
+                );
+            }
+            writeln!(writer)?;
+            writeln!(writer, "{}", res.text()?)?;
+        } else {
+            anyhow::bail!("no workaround found for crate {}", krate.name);
+        }
+        Ok(())
+    }
+}
+
+pub fn get_rs_deps<P: AsRef<Path>>(root: P) -> anyhow::Result<String> {
+    let root = root.as_ref();
+
+    let krates = get_all_crates(
+        Utf8Path::from_path(root.join("src-tauri/Cargo.toml").as_path()).unwrap(),
+        false,
+        false,
+        vec![],
+        false,
+        LockOptions {
+            frozen: false,
+            locked: true,
+            offline: false,
+        },
+        &Config {
+            ignore_build_dependencies: true,
+            ignore_dev_dependencies: true,
+            ..Default::default()
+        },
+        &[],
+    )?;
+
+    let store = cargo_about::licenses::store_from_cache()?;
+    let client = reqwest::blocking::Client::new();
+    let licenses = cargo_about::licenses::Gatherer::with_store(Arc::new(store)).gather(
+        &krates,
+        &Config {
+            ignore_build_dependencies: true,
+            ignore_dev_dependencies: true,
+            ..Default::default()
+        },
+        Some(client),
+    );
+
+    let accepted = vec![
+        Licensee::parse("MIT").unwrap(),
+        Licensee::parse("Apache-2.0").unwrap(),
+        Licensee::parse("MPL-2.0").unwrap(),
+        Licensee::parse("Unicode-3.0").unwrap(),
+        Licensee::parse("ISC").unwrap(),
+        Licensee::parse("BSD-3-Clause").unwrap(),
+    ];
+    let (_files, resolved) =
+        cargo_about::licenses::resolution::resolve(&licenses, &accepted, &BTreeMap::new(), true);
+
+    let real_deps = get_real_deps(root.join("src-tauri"))?;
+
+    let mut writer = std::io::BufWriter::new(Vec::new());
+    let workaround = DepWorkaround::load()?;
+    for (krate_license, resolved) in licenses.iter().zip(resolved.iter()) {
+        if krate_license.krate.name.starts_with("autd3") {
+            continue;
+        }
+
+        if !real_deps.iter().any(|(name, ver)| {
+            name == &krate_license.krate.name && ver == &krate_license.krate.version
+        }) {
+            continue;
+        }
+
+        writeln!(writer)?;
+        writeln!(
+            writer,
+            "---------------------------------------------------------"
+        )?;
+        writeln!(writer)?;
+        writeln!(
+            writer,
+            "{} {} ({})",
+            krate_license.krate.name,
+            krate_license.krate.version,
+            if let Some(r) = resolved {
+                if r.licenses.is_empty() {
+                    anyhow::bail!("no resolved licenses");
+                } else {
+                    r.licenses.iter().map(|f| format!("{f}")).join(" AND ")
+                }
+            } else {
+                anyhow::bail!("no resolved licenses");
+            }
+        )?;
+        if let Some(repo) = &krate_license.krate.repository {
+            writeln!(writer, "{}", repo)?;
+        }
+
+        let license_files = krate_license
+            .license_files
+            .iter()
+            .filter(|lf| {
+                lf.license_expr.evaluate(|req| {
+                    resolved.as_ref().is_some_and(|resolved| {
+                        resolved
+                            .licenses
+                            .iter()
+                            .any(|lr| req.license.id() == lr.license.id())
+                    })
+                })
+            })
+            .collect::<Vec<_>>();
+        if license_files.is_empty() {
+            workaround.find(&mut writer, krate_license.krate)?;
+        }
+        for lf in &license_files {
+            writeln!(writer)?;
+            if license_files.len() > 1 {
+                writeln!(writer, "---")?;
+            }
+            match &lf.kind {
+                cargo_about::licenses::LicenseFileKind::Text(text) => {
+                    writeln!(writer, "{}", text)?;
+                }
+                _ => {
+                    anyhow::bail!("unexpected license file kind");
+                }
+            }
+            if license_files.len() > 1 {
+                writeln!(writer, "---")?;
+            }
+        }
+        writeln!(
+            writer,
+            "---------------------------------------------------------"
+        )?;
+    }
+
+    Ok(String::from_utf8(writer.into_inner()?)?)
 }
